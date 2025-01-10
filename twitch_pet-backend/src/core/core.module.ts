f4355bb48565aebdb3540aabdc0232bc11e315e6
daniil <onlybmw17@gmail.com>
2025-01-10 10:12:26 +0300

    add backend base

---
+++ twitch_pet-backend/src/core/core.module.ts
@@ -0,0 +1,32 @@
+import { ApolloDriver } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+
+import { AccountModule } from '@/modules/auth/account/account.module';
+import { SessionModule } from '@/modules/auth/session/session.module';
+import { IS_DEV_ENV } from '@/shared/utils/is-dev.util';
+
+import { getGraphQLConfig } from './config/graphql.config';
+import { PrismaModule } from './prisma/prisma.module';
+import { RedisModule } from './redis/redis.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: !IS_DEV_ENV,
+      isGlobal: true,
+    }),
+    GraphQLModule.forRootAsync({
+      driver: ApolloDriver,
+      imports: [ConfigModule],
+      useFactory: getGraphQLConfig,
+      inject: [ConfigService],
+    }),
+    PrismaModule,
+    RedisModule,
+    AccountModule,
+    SessionModule,
+  ],
+})
+export class CoreModule {}
---
+++ twitch_pet-backend/src/modules/auth/account/account.service.ts
@@ -0,0 +1,52 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { hash } from 'argon2';
+
+import { PrismaService } from '@/core/prisma/prisma.service';
+
+import { CreateUserInput } from './inputs/create-user.input';
+
+@Injectable()
+export class AccountService {
+  public constructor(private readonly prismaService: PrismaService) {}
+
+  public async findAll() {
+    const users = await this.prismaService.user.findMany();
+
+    return users;
+  }
+
+  public async create(input: CreateUserInput) {
+    const { username, email, password } = input;
+
+    const isUsernameExists = await this.prismaService.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (isUsernameExists) {
+      throw new ConflictException('Это имя пользователя уже занято');
+    }
+
+    const isEmailExists = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (isEmailExists) {
+      throw new ConflictException('Эта почта уже занята');
+    }
+
+    await this.prismaService.user.create({
+      data: {
+        username,
+        email,
+        password: await hash(password),
+        displayName: username,
+      },
+    });
+
+    return true;
+  }
+}
